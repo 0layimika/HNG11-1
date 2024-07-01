99ce7b2ed387c066687cd15dc92cfb7f4094af07
0layimika <110624406+0layimika@users.noreply.github.com>
2024-07-01 11:18:56 +0100

    mehnnnnn

--- server.js
+++ server.js
@@ -15,27 +15,27 @@ app.get('/api/hello', async (req,res)=>{
     const visitor = req.query.visitor_name
     let client_ip = req.headers['cf-connecting-ip']||req.headers['x-real-ip']||req.headers['x-forwarded-for']||req.socket.remoteAddress
     res.json({client_ip})
-    // const api_key = config.get("api_key")
-    // const url = 'https://apiip.net/api/check?ip='+client_ip +'&accessKey='+ api_key
-    // const response = await axios.get(url)
-    // const result = response.data
-    // const location = result.city
-    // const openWeatherMapApiKey = '5b5e364ad73b6698554607ec8884f813';
-    //
-    // const weatherResponse = await axios.get('http://api.openweathermap.org/data/2.5/weather', {
-    //   params: {
-    //     lat: result.latitude,
-    //     lon: result.longitude,
-    //     appid: openWeatherMapApiKey,
-    //   },
-    // });
-    // const weatherdata = weatherResponse.data
-    // const temp = (weatherdata.main.temp - 273.15).toFixed(2)
-    //
-    // res.json({
-    //     "client_ip": client_ip,
-    //     "location": location,
-    //     "greeting":`Hello, ${visitor}!, the temperature is ${temp} degrees today in ${location}`,
-    // })
+    const api_key = config.get("api_key")
+    const url = 'https://apiip.net/api/check?ip='+client_ip +'&accessKey='+ api_key
+    const response = await axios.get(url)
+    const result = response.data
+    const location = result.city
+    const openWeatherMapApiKey = '5b5e364ad73b6698554607ec8884f813';
+
+    const weatherResponse = await axios.get('http://api.openweathermap.org/data/2.5/weather', {
+      params: {
+        lat: result.latitude,
+        lon: result.longitude,
+        appid: openWeatherMapApiKey,
+      },
+    });
+    const weatherdata = weatherResponse.data
+    const temp = (weatherdata.main.temp - 273.15).toFixed(2)
+
+    res.json({
+        "client_ip": client_ip,
+        "location": location,
+        "greeting":`Hello, ${visitor}!, the temperature is ${temp} degrees today in ${location}`,
+    })
 
 })
